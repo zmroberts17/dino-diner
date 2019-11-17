3664d2f34c2446d5a8bb0db75981f38f902fe6ac
Zane Roberts <zmroberts17@ksu.edu>
2019-11-17 10:14:13 -0600

    This commit should add the Website basics

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -73,7 +73,13 @@ namespace DinoDiner.Menu
             get
             {
                 List<Object> items = new List<Object>();
+                items.Add(new CretaceousCombo(new Brontowurst()));
+                items.Add(new CretaceousCombo(new DinoNuggets()));
+                items.Add(new CretaceousCombo(new PrehistoricPBJ()));
+                items.Add(new CretaceousCombo(new PterodactylWings()));
+                items.Add(new CretaceousCombo(new SteakosaurusBurger()));
                 items.Add(new CretaceousCombo(new TRexKingBurger()));
+                items.Add(new CretaceousCombo(new VelociWrap()));
                 return items;
             }
         }
---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public Menu Menu { get; } = new Menu();
+        public void OnGet()
+        {
+            
+        }
+    }
+}
